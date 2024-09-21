7a9136f2e80d0b5198250942477c2be729c93d40
AdrianMares <adrianmares.dev@gmail.com>
2024-09-21 04:05:28 -0400

    testing button functions

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-let currentSlide = 0;
-const itemsShown = 3;
+/*const itemsShown = 3;
 
 function moveSlide(direction) {
     const items = document.querySelectorAll(".featured-item");
@@ -16,4 +15,38 @@ function moveSlide(direction) {
 const offset = -currentSlide * (100 / itemsShown);
 const container = document.querySelector(".featured-container");
 
-container.style.transform = `translateX(${offset})`;
+container.style.transform = `translateX(${offset})`;*/
+
+const carouselItems = document.querySelectorAll(".featured-item");
+const leftButton = document.querySelector(".left-button");
+const rightButton = document.querySelector(".right-button");
+
+let currentSlide = 0;
+const itemsVisible = 3;
+
+function updateCarousel() {
+    carouselItems.forEach((item, index) => {
+        item.style.display = (index >= currentSlide && index < currentSlide + itemsVisible) ? 'block' : 'none';
+    })
+}
+
+rightButton.addEventListener("click", slideNext);
+leftButton.addEventListener("click", slidePrev);
+
+function slideNext() {
+    currentSlide += itemsVisible;
+    if(currentSlide >= carouselItems.length) {
+        currentSlide = 0;
+    }
+    updateCarousel();
+}
+
+function slidePrev() {
+    currentSlide -= itemsVisible;
+    if(currentSlide < 0) {
+        currentSlide = Math.max(0, carouselItems.length - itemsShown);
+    }
+    updateCarousel();
+}
+
+updateCarousel();
