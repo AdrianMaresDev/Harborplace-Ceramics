5067c6270ebbfa14ee168dbb910a931db2a91f9a
AdrianMares <adrianmares.dev@gmail.com>
2024-10-04 01:50:24 -0400

    added sidebar nav

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
+//NAVIGATION BAR
+
+const sidebar = document.querySelector('.sidebar');
+const menuIcon = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon');
+
+menuIcon.addEventListener('click', showSidebar);
+closeIcon.addEventListener('click', hideSidebar);
+
+function showSidebar() {
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    sidebar.style.display = 'none';
+}
+
+//CAROUSEL
+
 const carouselItems = document.querySelectorAll(".featured-item");
 const leftButton = document.querySelector(".left-button");
 const rightButton = document.querySelector(".right-button");
